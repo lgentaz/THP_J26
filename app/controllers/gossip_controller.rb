776bef741c61adc97e7269bc8de960d5d9ba0afb
lgentaz <lucile.gentaz@gmail.com>
2020-05-04 18:01:14 +0200

    message

---
+++ app/controllers/gossip_controller.rb
@@ -0,0 +1,4 @@
+class GossipController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'user/show'
+  
+  get 'gossip/show'
+
   root 'static_pages#home'
 
   get '/', to: 'static_pages#home'
