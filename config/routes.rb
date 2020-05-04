476fcbdfa8eefac24f9538db3ab917600a6be115
lgentaz <lucile.gentaz@gmail.com>
2020-05-04 17:21:28 +0200

    accueil

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
 
-  root 'welcome#show'
+  root 'static_pages#home'
+
+  get '/', to: 'static_pages#home'
   
   get 'welcome/:user_entry', to: 'welcome#show', as: 'user_entry'
 
   get '/team', to: 'static_pages#team'
 
   get '/contact', to: 'static_pages#contact'
+
+  get '/', to: 'welcome#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
